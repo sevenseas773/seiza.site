8d49bbce132f70435d9f5a9f780f1b1e575682c9
babygirl <nana_smnm@gmail.com>
2023-09-25 16:52:35 +0900

    responsible

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(function($) {
+
+    $(document).ready(function() {
+        $('#pagepiling').pagepiling({
+        });
+    });
+
     $(".button").modaal();
 
     $('a[href^="#"]').click(function(){
